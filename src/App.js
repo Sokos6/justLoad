dd120e40a670f1192a2e0f623eeaa6bff999da94
Sokos6 <will.sokolowski@gmail.com>
2020-02-01 22:45:15 -0600

    loader test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Loader from './Loader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        THIS IS THE TOP ONE
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <b>I AM HERE</b>
+
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
+    <Loader />
     </div>
-  );
+    )
+  }
 }
 
 export default App;
