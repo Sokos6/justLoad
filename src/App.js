60ba7e1d7eadd5108a52fa24545532d807ffea3f
Sokos6 <will.sokolowski@gmail.com>
2020-02-01 22:48:52 -0600

    clean up

--- src/App.js
+++ src/App.js
@@ -1,45 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Loader from './Loader';
 
 class App extends Component {
   render() {
-    return (
-      <div>
-        THIS IS THE TOP ONE
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <b>I AM HERE</b>
-
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    <Loader />
-    </div>
-    )
+    return <Loader />;
   }
 }
-
 export default App;
